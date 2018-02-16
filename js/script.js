c3c78049612ba0e7fe3be785ce0e41c0a0ded112
Andrey Kovalev <altaiman@gmail.com>
2018-02-16 15:17:20 +0700

    dropdown menu, modal, favicon, fixes

--- js/script.js
+++ js/script.js
@@ -35,4 +35,38 @@ $(function(){
         $('.mobile .open:not(.mobile__search)').removeClass('open');
         $(this).parent().toggleClass('open');
     })
+
+    $('[data-modal]').on('click', function() {
+        var id = $(this).data('modal');
+
+        $('#'+id).bPopup({
+            closeClass:'modal__close'
+        });
+    });
+
+    $('select').niceSelect();
+
+
+    $(window).on('scroll', function() {
+		if ($(window).scrollTop() >= 600) {
+			$('.f-block__img svg').each(function(i, svg) {
+		    	$(svg).addClass('animated');
+		    });
+		}
+    });
+
+    // блик на логотипе
+    setInterval(function() {
+    	$('.logo img').attr('src', 'img/logogif.gif')
+    }, 5000)
+
+    $('.f-block').hover(function(e) {
+        var tip = $(this).find('.f-block__tip').get(0);
+
+        if (tip) {
+            $(tip).fadeIn(200)
+        }
+    }, function(e) {
+        $('.f-block__tip').hide();
+    });
 });
